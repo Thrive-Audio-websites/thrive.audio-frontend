c644ba8e2d756115797553dbd6621f2a06fbae4a
root <cawasp@gmail.com>
2021-10-24 16:41:17 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function test() {
+    //document.getElementById("test").scrollIntoView(true);
+    //window.scroll({top:895, behavior: 'smooth'})
+    window.scrollBy(0, document.getElementById("first-product").getBoundingClientRect().top-60);
+    console.log(document.getElementById("first-product").getBoundingClientRect().top);
+}
+
+window.onscroll = function () {UpButton()};
+
+function UpButton() {
+    let button = document.getElementById("up_button");
+    let top = document.getElementById("button-pop_up");
+    let topY = top.offsetTop;
+    if (window.pageYOffset > topY) {
+        button.classList.remove("button-hide");
+    } else {
+        button.classList.add("button-hide");
+    }
+}
