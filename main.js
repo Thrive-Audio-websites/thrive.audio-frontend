73a1cf144d02862c295449668acd7d22595db18d
Alex <cawasp@gmail.com>
2021-10-24 19:35:41 -0700

    mobile compatibility improvements, mostly main menu

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 function scroll_to_products() {
     //document.getElementById("test").scrollIntoView(true);
     //window.scroll({top:895, behavior: 'smooth'})
-    window.scrollBy(0, document.getElementById("first-product").getBoundingClientRect().top-60);
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        window.scrollBy(0, document.getElementById("first-product").getBoundingClientRect().top-150);
+    } else {
+        window.scrollBy(0, document.getElementById("first-product").getBoundingClientRect().top-60);
+    }
     console.log(document.getElementById("first-product").getBoundingClientRect().top);
 }
 
-window.onscroll = function () {UpButton()};
+function menu_toggle() {
+    let menu_background = document.getElementById("menu-background");
+    let menu = document.getElementById("menu")
+    let menu_button = document.getElementById("menu_button")
+    if (menu.classList.contains("menu-expanded")) {
+        menu.classList.remove("menu-expanded")
+        menu_background.classList.remove("mobile-menu-button-background-expanded")
+    } else {
+        menu.classList.add("menu-expanded")
+        menu_background.classList.add("mobile-menu-button-background-expanded")
+    }
+}
 
-function UpButton() {
+window.onscroll = function () {scroll_events()};
+
+function scroll_events() {
     let button = document.getElementById("up_button");
     let top = document.getElementById("button-pop_up");
     let topY = top.offsetTop;
@@ -15,5 +32,12 @@ function UpButton() {
         button.classList.remove("button-hide");
     } else {
         button.classList.add("button-hide");
+    }
+
+    let menu_background = document.getElementById("menu-background");
+    if (window.pageYOffset > 0) {
+        menu_background.classList.add("menu-background-blur");
+    } else {
+        menu_background.classList.remove("menu-background-blur");
     }
 }
